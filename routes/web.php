4507fcc4e79f05dd9fc49a6ed3309e5a4ca7ef42
Thiago Nogueira Coelho <th.nogueira21@gmail.com>
2024-07-16 23:30:16 -0300

    Introdução a Controllers

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* sem utilizar um controller
+
+Route::get('/', function () {
+    return 'Página principal';
+});
+
+*/
+
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+
+Route::get('/contato', 'ContatoController@contato');
+
