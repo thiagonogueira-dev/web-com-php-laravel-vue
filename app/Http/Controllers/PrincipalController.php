c0e28a5c0455fb921e1b8dd5f95c2ac47d5326ab
Thiago Nogueira Coelho <th.nogueira21@gmail.com>
2024-07-17 00:43:09 -0300

    Introdução a rotas

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        echo 'Página principal';
+        return view('site.principal');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,9 +22,45 @@ Route::get('/', function () {
 
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() { return 'login'; })->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function() { return 'fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'produtos'; })->name('app.produtos');
+});
 
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. Clique <a href="'. route('site.index') . '"> aqui </a> para ir para a página inicial.';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* validação
+Route::get(
+    '/contato/{nome}/{categoria_id}', 
+    function(
+        string $nome = 'Desconhecido', 
+        int $categoria_id = 1 // 1 - Informação
+    ) {
+        echo 'Aqui, nome: ' . $nome . ' <br>Categoria: ' . $categoria_id;
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]{3}'); // categoria com id entre 0 e 9 com pelo menos um digito e nome com pelo menos 3 caracteres
+*/
