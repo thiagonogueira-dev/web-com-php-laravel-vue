25547e7024ab33dbb6c60e5ac6a58c75ca0a64f0
Thiago Nogueira Coelho <th.nogueira21@gmail.com>
2024-07-20 23:01:12 -0300

    Eloquent ORM

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    // Alterando o nome da tabela que o framework define por padrão
+    protected $table = 'fornecedores';
+    protected $fillable =['nome', 'site', 'uf', 'email'];
 }
